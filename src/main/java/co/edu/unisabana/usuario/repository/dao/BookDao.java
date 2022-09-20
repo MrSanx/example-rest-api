f293d4d091f37a57f6cf888fea05a2e3d89d90b7
MrSanx <sanchezestudios3111@gmail.com>
2022-09-19 22:32:46 -0500

    Implements Jacoco
    Add some test to get better test coverage.

--- src/main/java/co/edu/unisabana/usuario/repository/dao/BookDao.java
+++ src/main/java/co/edu/unisabana/usuario/repository/dao/BookDao.java
@@ -18,6 +18,48 @@ public class BookDao implements SearchBookPort, RegisterBookPort, AddBookPort {
     static List<BookEntity> listBooks = new ArrayList<>();
 
     @Override
+    public void registerBook(Book newBook) {
+        BookEntity bookEntity = BookEntity.fromModel(newBook);
+        if (validationRegisterBook(bookEntity)) {
+            bookEntity.setId(listBooks.size() + 1);
+            listBooks.add(bookEntity);
+        }
+    }
+
+    public boolean validationRegisterBook(BookEntity book) {
+        boolean validationAuthor = !book.getAuthor().equals("unknown");
+
+        if (validationAuthor) {
+            return true;
+        } else {
+            throw new IllegalArgumentException("El libro ya ha sido registrado.");
+        }
+    }
+
+    @Override
+    public boolean addBook(String name) {
+        for (BookEntity book : listBooks) {
+            if (book.getName().equals(name) && validationAddBook(book)) {
+                book.setQuantity(book.getQuantity() + 1);
+                return true;
+            }
+        }
+        // Para evitar confusiones futuras, se debe crear excepciones personalizadas
+        // conforme existan reglas de negocio
+        // que las ameriten. POR FAVOR AGREGUEN CONTEXTO
+        throw new PreliminaryRegisterException(name);
+    }
+
+    private boolean validationAddBook(BookEntity book) {
+        int max = 7;
+        if (book.getQuantity() <= max) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
     public boolean validateExistsBook(String nameBook) {
         AtomicBoolean exists = new AtomicBoolean(false);
         listBooks.forEach(book -> {
@@ -28,24 +70,11 @@ public class BookDao implements SearchBookPort, RegisterBookPort, AddBookPort {
         return exists.get();
     }
 
-
     @Override
-    public void registerBook(Book newBook) {
-        BookEntity bookEntity = BookEntity.fromModel(newBook);
-        bookEntity.setId(listBooks.size() + 1);
-        listBooks.add(bookEntity);
+    public List<BookEntity> getAuthorBooks(String name) {
+        List<BookEntity> authorBooks = new ArrayList<>();
+        listBooks.forEach(book -> { if (book.getAuthor().equals(name)) { authorBooks.add(book); } });
+        return authorBooks;
     }
 
-    @Override
-    public boolean addBook(String name) {
-        for (BookEntity book : listBooks) {
-            if (book.getName().equals(name)) {
-                book.setQuantity(book.getQuantity() + 1);
-                return true;
-            }
-        }
-        //Para evitar confusiones futuras, se debe crear excepciones personalizadas conforme existan reglas de negocio
-        //que las ameriten. POR FAVOR AGREGUEN CONTEXTO
-        throw new PreliminaryRegisterException(name);
-    }
 }
